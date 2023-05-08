5bce046438bc08e3e756db87e6bbe294fabc2767
Miguel Nieto Lopez <miguelnietolopez2002@gmail.com>
2023-05-07 20:51:59 -0500

    teststst

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 export default function home() {
   return (
     <>
-      <div id="homeTitleDiv" class="titleDiv homeDiv">
-        <h1 id="homeTitleH" class="homeEl titleH Header">Miguel Nieto Lopez</h1>
-        <h2 id="homeTitleSubH" class="homeEl titleSubH subHeader">Portfolio</h2>
+      <div id="homeTitleDiv" className="titleDiv homeDiv">
+        <h1 id="homeTitleH" className="homeEl titleH Header">Miguel Nieto Lopez</h1>
+        <h2 id="homeTitleSubH" className="homeEl titleSubH subHeader">Portfolio</h2>
       </div>
-      <div id="homeAboutDiv" class="homeDiv secDiv">
-        <h3 id="homeAboutTitle" class="homeEl Header">About Me</h3>
-        <div id="homeAboutCon" class="homeDiv homeCon ContentSec"></div>
+      <div id="homeAboutDiv" className="homeDiv secDiv">
+        <h3 id="homeAboutTitle" className="homeEl Header">About Me</h3>
+        <div id="homeAboutCon" className="homeDiv homeCon ContentSec"></div>
       </div>
-      <div id="homeSkillsDiv" class="homeDiv secDiv">
-        <h3 id="homeSkillsTitle" class="homeEl Header">Skills</h3>
-        <div id="homeSkillsCon" class="homeDiv homeCon ContentSec"></div>
+      <div id="homeSkillsDiv" className="homeDiv secDiv">
+        <h3 id="homeSkillsTitle" className="homeEl Header">Skills</h3>
+        <div id="homeSkillsCon" className="homeDiv homeCon ContentSec"></div>
       </div>
-      <div id="homeProjectsDiv" class="homeDiv secDiv">
-        <h3 id="homeProjectsDiv" class="homeEl Header">Projects</h3>
-        <div id="homeProjectsCon" class="homeDiv homeCon ContentSec"></div>
+      <div id="homeProjectsDiv" className="homeDiv secDiv">
+        <h3 id="homeProjectsDiv" className="homeEl Header">Projects</h3>
+        <div id="homeProjectsCon" className="homeDiv homeCon ContentSec"></div>
       </div>
     </>
   )
