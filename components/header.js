2a228ece8137cb8da89455ece096fe83e23b27df
Miguel Nieto Lopez <miguelnietolopez2002@gmail.com>
2023-05-29 18:47:49 -0500

    basic changes to add header

--- components/header.js
+++ components/header.js
@@ -1,6 +1,18 @@
-export default function header()
-{
+import Link from "next/link";
+
+export default function header() {
     <nav id="navbar">
-        <ul id="navbarList">{<}</ul>
+        <ul id="navbarList">
+            <div className="headerLink">
+                <Link href="/about">About</Link>
+            </div>
+            <div className="headerLink">
+                <Link href="/contact">Contact</Link>
+            </div>
+            <div className="headerLink">
+                <Link href="/projects">Projects</Link>
+            </div>
+
+        </ul>
     </nav>
 }
